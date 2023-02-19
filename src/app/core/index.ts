53e59fd66b7754ef522423e2c5b4a93e2b0c2dcf
Tony Hook <tonyhook.su@gmail.com>
2023-02-19 19:59:47 +0800

    [base] managed resource model
    managed resource: basic resource has owner, name and modify time
    sequence managed resource: resource with order
    hierarchy managed resource: resource with a hierarchy structure
    contained managed resource: resource is contained in another resource

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -9,5 +9,9 @@ export * from './layout/menu.service';
 
 export * from './api/entity/security/login';
 export * from './api/entity/security/user-details';
+export * from './api/entity/managed-resource';
+export * from './api/entity/sequence-managed-resource';
+export * from './api/entity/hierarchy-managed-resource';
+export * from './api/entity/contained-managed-resource';
 
 export * from './api/open/security.api';
